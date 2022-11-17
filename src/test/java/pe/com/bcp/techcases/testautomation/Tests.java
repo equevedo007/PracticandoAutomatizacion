9a374b7faafc3e88c463233d1e44640085a7fde3
Edinson Quevedo <quevedo.edinson@gmail.com>
2022-11-16 19:49:59 -0500

    Solution

--- src/test/java/pe/com/bcp/techcases/testautomation/Tests.java
+++ src/test/java/pe/com/bcp/techcases/testautomation/Tests.java
@@ -5,5 +5,5 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features="src/test/resources/features/", glue="pe.com.bcp.techcases.testautomation")
+@CucumberOptions(features="src/test/resources/features/api/findUser.feature", glue="pe.com.bcp.techcases.testautomation")
 public class Tests {}
